00373ae74b200e59c6a8b04d4997b99b4048e0d6
Jeff <salisbury.jeffery@gmail.com>
2020-03-17 10:06:58 -0400

    Adding new function to get modifier of a stat.

--- src/familiar.py
+++ src/familiar.py
@@ -4,3 +4,9 @@ def dice_roll(number_of_dice, dice_sides, mod):
     for i in range(1, number_of_dice + 1):
         total += random.randint(1, dice_sides)
     return total + mod
+
+
+def get_modifier(stat):
+    mod = stat - 10
+    mod = mod / 2
+    return round(mod)
